d28823682a9ab461004fd9c1d87c1b1618211666
Rohan Anand <thededlier@gmail.com>
2017-05-05 23:33:56 +0530

    bigdiscounts get list and add items

---
+++ dev/bigdiscounts/get_all.php
@@ -0,0 +1,16 @@
+<?php
+  include '../cors.php';
+  include '../credentials.php';
+
+  $sql = "SELECT * from bargain_items where status=1 order by starttime";
+
+  $result = $conn->query($sql);
+
+  if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $r[] = $row;
+    }
+  }
+
+  echo json_encode($r);
+?>
