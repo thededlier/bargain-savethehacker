1b985d83798138185016ffa239da43a9e3c0fc46
Rohan Anand <thededlier@gmail.com>
2017-05-06 00:24:45 +0530

    sigin api

--- dev/bigdiscounts/signup.php
+++ dev/bigdiscounts/signup.php
@@ -4,19 +4,22 @@ include './cors.php';
 if($_SERVER['REQUEST_METHOD'] == "POST"){
 	// Get data
 	$name = $_POST['name'];
+  $shop = $_POST['shop'];
+  $address = $_POST['address'];
 	$email = $_POST['email'];
 	$password = $_POST['pwd'];
 	$vid = uniqid("VD");
 
-	$sql = "INSERT INTO vendors(vid, name, email, password) VALUES ('$vid', '$name', '$email', '$password')";
+	$sql = "INSERT INTO vendors(vid, name, shop, address, email, password)
+          VALUES ('$vid', '$name', '$shop', '$address', '$email', '$password')";
 
   if($conn->query($sql)){
-		$json = array("status" => 200, "msg" => "Success");
+		$json = array("status" => 200, "message" => "Success");
 	}else{
-		$json = array("status" => 400, "msg" => $conn->error);
+		$json = array("status" => 400, "message" => $conn->error);
 	}
 }else{
-	$json = array("status" => 400, "msg" => "Request method not accepted");
+	$json = array("status" => 400, "message" => "Request method not accepted");
 }
 
 /* Output header */
