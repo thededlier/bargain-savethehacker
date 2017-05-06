57a03456fb994917018a7fc6c1fc8102698e6746
Rohan Anand <thededlier@gmail.com>
2017-05-06 13:55:57 +0530

    notify of goal reach

---
+++ dev/bigdiscounts/notify_goal_reach.php
@@ -0,0 +1,16 @@
+<?php
+    $html = "";
+
+    $sql = "SELECT * FROM bargainbin where vemail = $email AND goal >= reach AND alerted = 0";
+
+    $result = $conn->query($sql);
+
+    if($reult->num_rows > 0) {
+        while($rows = $result->fetch_assoc()) {
+            $html .=
+                '<div class="alert alert-info">
+                    <strong>Information!</strong> A goal has been reached on a product <a href="details.php?pid=' . $pid . '" class="alert-link"> ' . $pid . '</a>.
+                </div>';
+        }
+    }
+?>
