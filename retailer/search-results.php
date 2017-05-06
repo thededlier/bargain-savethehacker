0661f9afb7dc274c98c283cdbaee38942c0e65fe
Shivam Saxena <Shivam Saxena>
2017-05-06 10:58:10 +0530

    styling in result page

--- retailer/search-results.php
+++ retailer/search-results.php
@@ -14,10 +14,7 @@
 		<title>bar-gain</title>
 
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Crete+Round|Revalia|Arvo|Pavanam" rel="stylesheet">
-		<link href="css/mystyle.css" rel="stylesheet">
-        <link href="css/mystyles.css" rel="stylesheet">
+    	<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -66,6 +63,7 @@
 				</div>
 			</div>
         </nav>
+        <div style="margin-top: 150px">
 
 		<div class="row">
             <div class="container">
@@ -76,6 +74,7 @@
 						itemSearch($searchIndex, $keywords);
 					}
 				?>
+            </div>
             </div>
 		</div>
 	</body>
